2561f9cc8ace06eb885b5e3e10b66cde03e82d9c
Jose <jose_chester.96@hotmail.com>
2018-03-19 17:36:34 -0600

    is alive

---
+++ app/src/main/java/com/simcoder/uber/PayPalConfig.java
@@ -0,0 +1,9 @@
+package com.simcoder.uber;
+
+/**
+ * Created by jose on 3/19/18.
+ */
+
+class PayPalConfig {
+    public static String PAYPAL_CLIENT_ID="AdFV7evYl-mc4M1ddqo7D_8CycTFLxrVd474YT_Yi11IsSLgYFSlXgva7DwLSn-oW_31DKaaqTWRuDOK";
+}
